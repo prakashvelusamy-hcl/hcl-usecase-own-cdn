cba39915d16364fe099a613dd84d6f6461277419
prakashvelusamy-hcl <prakashvelusamy1999@gmail.com>
2025-06-17 08:07:49 +0530

    fix

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "aws-state-s3"
     key            = "hcl-usecase-own/terraform.tfstate"
-    profile        = "devops"
+    #profile        = "devops"
     region         = "ap-south-1"                
     encrypt        = true                         
   }
